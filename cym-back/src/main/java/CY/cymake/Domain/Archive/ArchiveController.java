a8a508dece4825fcf2efd2298a94cfeea24df5de
subin930 <bread0930@g.skku.edu>
2024-07-29 11:20:49 +0900

    지식 아카이브 검색 api 수정

--- cym-back/src/main/java/CY/cymake/Domain/Archive/ArchiveController.java
+++ cym-back/src/main/java/CY/cymake/Domain/Archive/ArchiveController.java
@@ -6,10 +6,7 @@ import CY.cymake.Response.GlobalResponseHandler;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
@@ -44,7 +41,7 @@ public class    ArchiveController {
      */
     @Operation(description = "주제 별 뉴스 기사 검색")
     @GetMapping(value = "/total/{subject}/search")
-    public CommonResult<List<NewsResDto>> searchNews(@PathVariable(value = "subject") String subject, String searchBody) throws IOException {
+    public CommonResult<List<NewsResDto>> searchNews(@PathVariable(value = "subject") String subject, @RequestParam(value = "searchBody") String searchBody) throws IOException {
         return globalResponseHandler.SendSuccessAndContent(archiveService.searchNews(subject, searchBody));
     }
 }
