9d2fbe25c5f9630b813b93ccf30bbfb4e72e1380
Park Seo yeong <olivia72951@gmail.com>
2024-08-06 14:34:01 +0900

    뉴스 요약 및 키워드 관련 UI 개선

--- cym-front/src/components/common/NewsModal.vue
+++ cym-front/src/components/common/NewsModal.vue
@@ -31,10 +31,12 @@ onMounted(() => {
                 <br>
                 
                 <p class="mt-2" style="color: #7248BD; font-weight: bold">뉴스 요약</p>
-                <ul>
-                    <li v-for="summaryItem in summary" :key="summaryItem" class="m-2" style="font-size: .9rem;">{{ summaryItem }}</li>
+                
+                <ul class="list-group m-2">
+                    <li v-for="summaryItem in summary" :key="summaryItem" class="list-group-item px-2" style="font-size: .9rem;">{{ summaryItem }}</li>
                 </ul>
-                <a :href="newsLink" class="news-link" style="font-size: .9rem; font-weight: bold" target="_blank" rel="noopener noreferrer">뉴스 원문 보기 <i class="bi bi-box-arrow-up-right"></i></a>
+                <p class="news-keyword ms-3" style="color: #7248BD; font-weight: bold; font-size: .9rem;"> {{ keywords.join(' / ') }} </p>
+                <a :href="newsLink" class="news-link mt-2" style="font-size: .9rem; font-weight: bold" target="_blank" rel="noopener noreferrer">뉴스 원문 보기 <i class="bi bi-box-arrow-up-right"></i></a>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
@@ -57,5 +59,8 @@ onMounted(() => {
 }
 .news-link:hover {
   color: #7248BD;
+}
+.modal-footer {
+    border-top: none; /* 모달 푸터의 상단 선 제거 */
 }
 </style>
