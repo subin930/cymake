6b00ba7f5ba2c1183d26eb702fab79d8a59b948f
Park Seo yeong <olivia72951@gmail.com>
2024-08-07 11:11:03 +0900

    봇 응답 생성 중 메세지 추가

--- cym-front/src/components/common/Chat.vue
+++ cym-front/src/components/common/Chat.vue
@@ -4,33 +4,35 @@ import axios from 'axios';
 
 const token = localStorage.getItem("token");
 const sessionId = ref(localStorage.getItem("sessionId"));
+const uploadedFile= ref(null);
+
 // Chat state (local에 저장할지 session에 저장할지)
 const messages = ref(JSON.parse(localStorage.getItem('chatMessages')) || []);
 const newMessage = ref('');
 const botMessage = ref('');
 const chatMessages = ref(null);
-// Watcher to save messages to sessionStorage whenever messages change
+const botLoading = ref(false);
+
+// message 추가 시 바로 스토리지에 저장될 수 있도록
 watch(messages, (newMessages) => {
     localStorage.setItem('chatMessages', JSON.stringify(newMessages));
 }, { deep: true });
 
-const uploadedFile= ref(null);
-// Function to open the chat offcanvas
 const openChat = () => {
   const chatOffcanvas = new bootstrap.Offcanvas(document.getElementById('chatOffcanvas'));
   chatOffcanvas.show();
 };
 
-// Function to close the chat offcanvas
+
 const closeChat = () => {
   const chatOffcanvas = bootstrap.Offcanvas.getInstance(document.getElementById('chatOffcanvas'));
   chatOffcanvas.hide();
 };
 
-// Function to send a message
+//챗봇에게 메세지 전송
 const sendMessage = async() => {
   if (newMessage.value.trim()) {
-    // Add user message to chat
+    //메세지 목록에 유저 메세지 추가
     messages.value.push({ text: newMessage.value + uploadedFile.value, sender: 'user' });
     console.log(sessionId);
     let formData = new FormData();
@@ -45,6 +47,7 @@ const sendMessage = async() => {
     document.getElementById('file').value = null;
     scrollToBottom();
     try {
+      botLoading.value = true;
       const response = await axios.post(`/v1/chat/question`,  formData, {
         headers: {
           'Authorization': `Bearer ${token}`,
@@ -62,8 +65,8 @@ const sendMessage = async() => {
       console.error('sendMessage error:', error);
       botMessage.value = '오류가 발생했습니다';
     }
-  
-    // Simulate sending to a chatbot and receiving a response
+    botLoading.value = false;
+    // 챗봇 답변 추가
     messages.value.push({ text: botMessage.value, sender: 'bot' });
     scrollToBottom();
   }
@@ -105,6 +108,7 @@ onMounted(scrollToBottom);
                     class="message" :class="{'user': message.sender === 'user', 'bot': message.sender === 'bot'}">
                     {{ message.text }}
                     </div>
+                    <div v-if="botLoading===true" class="message bot" style="font-size: .9rem; color: #212121;">응답 생성 중입니다...</div>
                 </div>
                 <!-- Input for new message -->
                 <div class="container chat-input mb-2">
