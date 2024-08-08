31748e94bebc1e9ac7dd971ba59492db6c63f02e
Sayper <paper7459@gmail.com>
2024-08-08 14:56:18 +0900

    chat.py 잔버그 수정

--- cym-llm/chat.py
+++ cym-llm/chat.py
@@ -20,7 +20,7 @@ def doChat(company_code, session_id, message):
     system_prompt = "This line explains how you should answer the chat inputs. First of all, you should answer only in Korean. The very next line of input is the 'Company Code'. 'Company Code' refers to the folder name in the 'files/' directory on your knowledge base. In other words, if the company code is 123, it refers to the files/123/'. When you refer to the knowledge base, you should only refer to the 'crawl/' and its lower directories and the directory that fits your company code like 'files/123' if your company code is 123, and files in other directories should never be referred to in the answer generation, even if you need the information. The contents of the chat entered by the user will come out from the next line of the company code.\n"
 
     try:
-        knowledge_base_id = bedrock_id[company_code]
+        knowledge_base_id = bedrock_id[company_code][0]
     except KeyError:
         raise KeyError(f"The company code '{company_code}' does not exist in the bedrock_id dictionary.")
     
