da2f3e88edaa5f6f5a7c90e0e6011530cd732889
subin930 <bread0930@g.skku.edu>
2024-10-28 23:30:48 +0900

    이메일 검증 API 추가

--- cym-back/src/main/java/CY/cymake/Config/SecurityConfig.java
+++ cym-back/src/main/java/CY/cymake/Config/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
             "/v1/users/register",
+            "/v1/users/mailSend",
+            "/v1/users/mailCheck",
             "/swagger-ui/**",
             "/v3/api-docs/**",
             "/v1/auth/**",
